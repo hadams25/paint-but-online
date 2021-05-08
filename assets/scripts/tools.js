3f3f986fd7fdfd10a16cadeac3244a9549a719db
hadams25 <46791347+hadams25@users.noreply.github.com>
2021-05-08 01:17:48 -0500

    add tool class

--- assets/scripts/tools.js
+++ assets/scripts/tools.js
@@ -2,5 +2,28 @@
 
 class tools
 {
-    
+    constructor(name)
+    {
+        this.name = name;
+    }
+
+    on_start()
+    {
+        this.populate_options();
+    }
+
+    on_left_click()
+    {
+        console.log("tool: " + String(this.name) + " left click event");
+    }
+
+    on_right_click()
+    {
+        console.log("tool: " + String(this.name) + " right click event");
+    }
+
+    populate_options()
+    {
+        document.getElementById("options-box").textContent = "NONE"
+    }
 }
